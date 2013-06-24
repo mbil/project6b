046e4626b0df1fde63e9de61e46ab3436457bcbb
mpruijssers <pruijssers.m@gmail.com>
2013-06-24 22:31:40 +0200

    Login verwijderd, opmaak verbeterd

---
+++ Huisgenoten/Huisgenoten/StartUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartUpViewController.h
+//  Huisgenoten
+//
+//  Created by Miguel Pruijssers on 24-06-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartUpViewController : UIViewController
+
+@end
