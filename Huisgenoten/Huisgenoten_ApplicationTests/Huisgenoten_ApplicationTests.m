4402d124ed4ce961fff2e42961571e59f33ce0b2
Myrthe Bil <myrthe.bil@student.uva.nl>
2013-06-25 22:23:42 +0200

    adjustments

--- Huisgenoten/Huisgenoten_ApplicationTests/Huisgenoten_ApplicationTests.m
+++ Huisgenoten/Huisgenoten_ApplicationTests/Huisgenoten_ApplicationTests.m
@@ -9,12 +9,14 @@
 #import "Huisgenoten_ApplicationTests.h"
 #import "AppDelegate.h"
 #import "ViewController.h"
+#import "ShoppinglistViewController.h"
+#import "AddItemViewController.h"
 
 @interface Huisgenoten_ApplicationTests ()
 
 //private properties
 @property (nonatomic, readwrite, weak) AppDelegate *appDelegate;
-@property (strong, nonatomic) ViewController *controller;
+@property (strong, nonatomic) AddItemViewController *controller;
 @property (nonatomic, readwrite, weak) UIWindow *window;
 
 @end
@@ -29,9 +31,19 @@
 {
     [super setUp];
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Mainstoryboard" bundle:nil];
-    self.controller = [storyboard instantiateViewControllerWithIdentifier:@"Huisgenoten"];
+    self.controller = [storyboard instantiateViewControllerWithIdentifier:@"AddItem"];
     [self.controller performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
     self.appDelegate = [[UIApplication sharedApplication] delegate];
+    
+}
+
+- (void) testAppDelegate
+{
+    STAssertNotNil(self.appDelegate, @"Cannot find the application delegate");
+}
+
+-(void) testAddItem {
+    [self.controller.nameField performSelectorOnMainThread:@selector(setText:) withObject:@"foo" waitUntilDone:YES];
 }
 
 @end
